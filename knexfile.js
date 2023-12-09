dbae21da4e08fd25c34ddbe7fd5a8013aad75945
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2023-12-09 18:48:42 -0500

    Completed Migrations and Seeding of Database

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   client: "mysql",
   connection: {
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DBNAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     charset: "utf8",
