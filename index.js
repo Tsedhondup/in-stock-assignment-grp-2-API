f61c75b8b07fdf3df3b6e5d788929026ff3f384c
Madison <madisonjohnson301@gmail.com>
2023-12-10 11:43:10 -0500

    instal cors and add middleware for front end use of data

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 const warehouseRoutes = require('./routes/warehouse-routes')
 
 app.use(express.json());
+
+// Use CORS middleware
+app.use(cors());
 
 // all warehouse routes
 app.use('/warehouses', warehouseRoutes);
